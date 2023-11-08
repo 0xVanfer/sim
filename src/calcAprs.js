71f17ada7c2dcac9c8774e5a9a8a45c4dadd3d17
0xVanfer <vanfer@vanfer.tech>
2023-11-08 18:45:25 +0800

    [fix] apr

--- src/calcAprs.js
+++ src/calcAprs.js
@@ -37,9 +37,9 @@ function CalcBestApr(){
 
     let vtps = []
     for (let vtpNum = 0; vtpNum < vtpLength; vtpNum++) {
-        let mar = parseFloat(document.getElementById("mar"+String(vtpNum)).textContent)
+        let mar = parseFloat(document.getElementById("mar"+String(vtpNum)).textContent)/100
         let credit = parseFloat(document.getElementById("credit"+String(vtpNum)).textContent)
-        let apr = parseFloat(document.getElementById("aprmax"+String(vtpNum)).textContent)
+        let apr = parseFloat(document.getElementById("aprmax"+String(vtpNum)).textContent)/100
 
         let vtpInfo = new Map()
         vtpInfo.set("id",vtpNum)
@@ -62,29 +62,49 @@ function CalcBestApr(){
         for (let j = 0; j < comb.length; j++){
             credit += comb[j].get("credit")
         }
-        if (credit > totalcredits){
-            continue
+        if (credit <= totalcredits){
+            combsRemain.push(comb)
         }
-        combsRemain.push(comb)
     }
-    SetError("combsRemain: "+combsRemain.length)
+    SetError("combs, combsRemain: "+combs.length+" "+combsRemain.length)
 
     let maxApr = 0.0
     for (let i = 0; i < combsRemain.length; i++){
-        let comb = combs[i]
+        // The comb info.
+        let comb = combsRemain[i]
+        // The apr can be provided by this comb.
         let combApr = 0
+        
         if (comb.length==1){
+            // Only one vtp in this comb.
             combApr = comb[0].get("apr")
         }else {
+            // Over 2 vtps in this comb.
             for (let j = 0; j < comb.length; j++){
-                let info = comb[j]
-                combApr += info.get("apr") * info.get("mar")
+                combApr += comb[j].get("apr") * comb[j].get("mar")
             }
         }
-        if (combApr > maxApr) maxApr = combApr
+        // This is the best so far.
+        if (combApr > maxApr) {
+            maxApr = combApr
+            // Set all the results to 0 first.
+            for (let vtpNum = 0; vtpNum < vtpLength; vtpNum++){
+                SetPercentage("allorate"+String(vtpNum), 0, 0)
+                SetPercentage("aprprov"+String(vtpNum), 0, 0)
+            }
+            // Set the res.
+            for (let j = 0; j < comb.length; j++){
+                let idStr = comb[j].get("id")
+                let mar = comb.length == 1 ? 1 : comb[j].get("mar");
+                let apr = comb[j].get("apr")
+
+                SetPercentage("allorate" + idStr, mar, 0)
+                SetPercentage("aprprov" + idStr, apr * mar, decimals)
+            }
+        }
     }
 
-    SetPercentage("bestapr", maxApr/100, decimals)
+    SetPercentage("bestapr", maxApr, decimals)
 
     return
 }
