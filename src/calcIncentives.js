99d58a3a675ed77ae89cbb722e11c298bd6335f3
0xVanfer <vanfer@vanfer.tech>
2023-11-08 16:37:37 +0800

    [feat] update incentives

--- src/calcIncentives.js
+++ src/calcIncentives.js
@@ -1,65 +1,52 @@
-
-let baseIDs = ["emission", "startt", "endt", "lia", "unpr", "inpr", "credit", "mar"]
-
-function ReadElements() {
-    let info = new Map();
-    info.set("mapName", "elements");
-
+function CalcApr(){
     let decimals = parseFloat(document.getElementById("decimals").textContent)
-    info.set("decimals", decimals);
-
     let timestampNow = parseFloat(document.getElementById("timestampnow").textContent)
-    info.set("timestampnow", timestampNow);
-    info.set("totalcredits", parseFloat(document.getElementById("totalcredits").textContent));
 
-    for (let vtpNum = 0; ; vtpNum++) {
+    for (let vtpNum = 0; vtpNum < 3; vtpNum++) {
+        let emission = parseFloat(document.getElementById("emission"+String(vtpNum)).textContent)
+        let startt = parseFloat(document.getElementById("startt"+String(vtpNum)).textContent)
+        let endt = parseFloat(document.getElementById("endt"+String(vtpNum)).textContent)
+        let lia = parseFloat(document.getElementById("lia"+String(vtpNum)).textContent)
+        let unpr = parseFloat(document.getElementById("unpr"+String(vtpNum)).textContent)
+        let inpr = parseFloat(document.getElementById("inpr"+String(vtpNum)).textContent)
+
+        let apr = 0;
+        if (!(emission==0 || lia == 0 || startt > timestampNow || endt < timestampNow)){
+            apr = emission * 86400 * 365 * inpr / lia / unpr
+        }
+
+        SetTime("start"+String(vtpNum),startt)
+        SetTime("end"+String(vtpNum),endt)
+        SetPercentage("apr" + String(vtpNum), apr, decimals)
+    }
+}
+
+function CalcBestApr(){
+    let decimals = parseFloat(document.getElementById("decimals").textContent)
+    let totalcredits = parseFloat(document.getElementById("totalcredits").textContent)
+
+    let vtpLength = 0;
+    for (let vtpNum = 0; vtpNum < 100; vtpNum++) {
         try{
-            let vtpInfo = new Map();
-            for (let i = 0; i < baseIDs.length; i++) {
-                let baseID = baseIDs[i]
-                let completeID = baseID + String(vtpNum)
-                vtpInfo.set(baseID, parseFloat(document.getElementById(completeID).textContent));
-            }
-
-            if (vtpInfo.get("emission") == 0 || vtpInfo.get("lia") == 0 || vtpInfo.get("startt") > timestampNow || vtpInfo.get("endt") < timestampNow){
-                vtpInfo.set("apr", 0);
-            } else {
-                let emissionPerYear = vtpInfo.get("emission") * 86400 * 365
-                let apr = emissionPerYear * vtpInfo.get("inpr") / (vtpInfo.get("lia") * vtpInfo.get("unpr"))
-                vtpInfo.set("apr", apr);
-            }
-            SetPercentage("apr" + String(vtpNum), vtpInfo.get("apr"), decimals)
-            info.set("vtpInfo"+String(vtpNum), vtpInfo);
-
-        } catch {
-            info.set("vtpLength",vtpNum)
-            break;
+            document.getElementById("mar"+String(vtpNum)).textContent
+        }catch{
+            vtpLength = vtpNum
+            break
         }
     }
 
-    return info
-}
-
-function SetNumber(id, value, decimals){
-    document.getElementById(id).innerHTML = value.toFixed(decimals);
-}
-
-function SetPercentage(id, value, decimals){
-    document.getElementById(id).innerHTML = (value*100).toFixed(decimals)+"%";
-}
-
-function SetError(errorStr){
-    document.getElementById("error").innerHTML = errorStr;
-}
-
-function CalcBestApr(info){
-    let bestAprInfo = new Map();
-    let vtpLength = info.get("vtpLength")
-
     let vtps = []
     for (let vtpNum = 0; vtpNum < vtpLength; vtpNum++) {
-        let index = "vtpInfo" + String(vtpNum)
-        let vtpInfo = info.get(index)
+        let mar = parseFloat(document.getElementById("mar"+String(vtpNum)).textContent)
+        let credit = parseFloat(document.getElementById("credit"+String(vtpNum)).textContent)
+        let apr = parseFloat(document.getElementById("aprmax"+String(vtpNum)).textContent)
+
+        let vtpInfo = new Map()
+        vtpInfo.set("id",vtpNum)
+        vtpInfo.set("mar",mar)
+        vtpInfo.set("credit",credit)
+        vtpInfo.set("apr",apr)
+
         vtps.push(vtpInfo)
     }
     SetError("vtps: "+vtps.length)
@@ -67,22 +54,22 @@ function CalcBestApr(info){
     let combs = GetAllCombinations(vtps)
     SetError("combs: "+combs.length)
 
+    // Filter by credit cost.
     let combsRemain = []
     for (let i = 0; i < combs.length; i++){
         let comb = combs[i]
         let credit = 0;
         for (let j = 0; j < comb.length; j++){
-            let info = comb[j]
-            credit += info.get("credit")
+            credit += comb[j].get("credit")
         }
-        if (credit > info.get("totalcredits")){
+        if (credit > totalcredits){
             continue
         }
         combsRemain.push(comb)
     }
     SetError("combsRemain: "+combsRemain.length)
 
-    let maxApr = 0
+    let maxApr = 0.0
     for (let i = 0; i < combsRemain.length; i++){
         let comb = combs[i]
         let combApr = 0
@@ -97,10 +84,9 @@ function CalcBestApr(info){
         if (combApr > maxApr) maxApr = combApr
     }
 
-    bestAprInfo.set("bestapr", maxApr)
-    SetPercentage("bestapr", maxApr)
+    SetPercentage("bestapr", maxApr/100, decimals)
 
-    return bestAprInfo
+    return
 }
 
 
@@ -108,7 +94,7 @@ function CalcBestApr(info){
 // Get combs of all vtps.
 function GetAllCombinations(arr){
     SetError("get combinations error")
-    
+
     let result = [];
     for (let i = 0; i < arr.length; i++) {
         let x = _getCombinations(arr, i + 1)
@@ -154,33 +140,27 @@ function Timestamp2Time(timestamp){
     return formattedDate + " " + formattedTime
 }
 
-// Set the timestamp to time.
-function ProcessTime(info){
-    SetError("process time error")
-
-    document.getElementById("timenow").innerHTML = Timestamp2Time(info.get("timestampnow"));
-
-    for (let vtpNum = 0; vtpNum < info.get("vtpLength"); vtpNum++) {
-
-        let timestampStart = info.get("vtpInfo"+String(vtpNum)).get("startt");
-        let timestampEnd = info.get("vtpInfo"+String(vtpNum)).get("endt");
-
-        let timeStartID = "start"+String(vtpNum);
-        let timeEndID = "end"+String(vtpNum);
-
-        document.getElementById(timeStartID).innerHTML = Timestamp2Time(timestampStart);
-        document.getElementById(timeEndID).innerHTML = Timestamp2Time(timestampEnd);
-    }
+function SetNumber(id, value, decimals){
+    document.getElementById(id).innerHTML = value.toFixed(decimals);
 }
 
+function SetPercentage(id, value, decimals){
+    document.getElementById(id).innerHTML = (value*100).toFixed(decimals)+"%";
+}
+
+function SetTime(id, timestamp){
+    document.getElementById(id).innerHTML = Timestamp2Time(timestamp);
+}
+
+function SetError(errorStr){
+    document.getElementById("error").innerHTML = errorStr;
+}
 
 
 function CalcIncentives(){
-    let info = ReadElements();
-    ProcessTime(info)
-
-    let bestAprInfo = CalcBestApr(info)
-
+    CalcApr()
+    CalcBestApr()
     SetError("success")
 }
+
 
